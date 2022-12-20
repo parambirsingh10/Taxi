dd25c4314300ff716be7948b20d9d5bd221b0de4
Navneet <namankaur1313@gmail.com>
2022-12-20 11:16:05 +0530

    commit all files

--- CI/app/Views/pages/Contact.php
+++ CI/app/Views/pages/Contact.php
@@ -14,8 +14,7 @@
       <nav class="navbar bg-light">
         <div class="container">
           <a class="navbar-brand" href="#">
-            <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
-          </a>
+            <img src="https://media.istockphoto.com/id/1300267568/vector/letter-p-fast-speed-motion-logo.jpg?s=612x612&w=is&k=20&c=lHCyBjhoOzHHFVpdLXO8tSyiUohIb1XqvZLptoB1Yxc=" alt="bootstrap" width="30" height="24">          </a>
         </div>
       </nav>
         <div class="container-fluid">
